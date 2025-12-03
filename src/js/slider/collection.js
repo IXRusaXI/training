be7c35067eddc21797f7e82e4e9b4d2a7a2877a0
Руслан <moi-denga@yandex.ru>
2025-12-03 23:51:26 +0300

    Исправил отображение изображений

--- src/js/slider/collection.js
+++ src/js/slider/collection.js
@@ -7,7 +7,6 @@ export let elementCollection = {
     showButton: document.querySelector('.drop-down-area__toggle-button'),
     hiddenLinksList: [],
     hidden: true,
-// ➡️ НОВЫЕ ЭЛЕМЕНТЫ ДЛЯ БЛОКА ТЕХНИКИ (Обновите селекторы!)
   deviceDropDown: document.querySelector('.devices-repair .drop-down-area'),
   deviceSlider: document.querySelector('.devices-repair .swiper-wrapper'),
   deviceFirstBlock: document.querySelector('.devices-repair .link-list'),
@@ -23,25 +22,25 @@ export let elementCollection = {
 
 export let logoImageCollection =
     [
-        '../img/logos/samsung-logo.png',
-        '../img/logos/acer-logo.png',
-        '../img/logos/apple-logo.png',
-        '../img/logos/bosch-logo.png',
-        '../img/logos/hp-logo.png',
-        '../img/logos/lenovo-logo.png',
-        '../img/logos/sony-logo.png',
-        '../img/logos/viewsonic-logo.png',
+        './img/logos/samsung-logo.png',
+        './img/logos/acer-logo.png',
+        './img/logos/apple-logo.png',
+        './img/logos/bosch-logo.png',
+        './img/logos/hp-logo.png',
+        './img/logos/lenovo-logo.png',
+        './img/logos/sony-logo.png',
+        './img/logos/viewsonic-logo.png',
     ]
 
 export let deviceTypeCollection = [
-  { name: 'Ремонт ноутбуков', image: '../img/icons/laptop-icon.svg' },
-  { name: 'Ремонт планшетов', image: '../img/icons/tablet-icon.svg' },
-  { name: 'Ремонт ПК', image: '../img/icons/desktop-icon.svg' },
-  { name: 'Ремонт мониторов', image: '../img/icons/monitor-icon.svg' },
-  { name: 'Ремонт смартфонов', image: '../img/icons/phone-icon.svg' },
-  { name: 'Ремонт принтеров', image: '../img/icons/printer-icon.svg' },
-  { name: 'Ремонт проекторов', image: '../img/icons/projector-icon.svg' },
-  { name: 'Ремонт серверов', image: '../img/icons/server-icon.svg' },
+  { name: 'Ремонт ноутбуков' },
+  { name: 'Ремонт планшетов'},
+  { name: 'Ремонт ПК' },
+  { name: 'Ремонт мониторов'},
+  { name: 'Ремонт смартфонов'},
+  { name: 'Ремонт принтеров'},
+  { name: 'Ремонт проекторов'},
+  { name: 'Ремонт серверов'},
 ];
 export let costCollection = [
   { title: 'Диагностика', cost: 'Бесплатно', time: '30 мин'  },
